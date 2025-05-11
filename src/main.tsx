971f9c968f3b6705e5185e490a97f7a4807da0d9
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-05-11 17:20:53 +0330

    try to add mask

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Range from "./range";
+import JalaaliDateInput from "./range/core/mask";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Range device="desktop" locale="fa" type="date" />
+    <Range device="desktop" locale="fa" type="range" />
+    <JalaaliDateInput />
   </StrictMode>
 );
--- src/range/core/mask.tsx
+++ src/range/core/mask.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import type { ChangeEvent } from "react";
-import { MaskedInput } from "antd-mask-input";
-import moment from "moment-jalaali";
-
+import type { ChangeEvent, InputHTMLAttributes } from "react";
+import moment, { type MomentInput } from "moment-jalaali";
 import type { IDate } from "./type";
 
 type MaskProps = {
@@ -16,17 +14,18 @@ type MaskProps = {
   locale: "fa" | "en";
 };
 
-const Mask = ({
+const DateMask = ({
   dateFromOutside,
   InputHandleChange,
-  value,
+  value: defaultValue = 14040202,
   className,
   locale,
   tertiaryColor = "#939393",
   secondaryColor = "#585858",
   dangerColor = "#f87171",
 }: MaskProps) => {
-  const [isEdit, setIsEdit] = useState(false);
+  const [value, setValue] = useState(defaultValue);
+  const [isEdit, setIsEdit] = useState(true);
   const editor = useRef<HTMLInputElement>(null);
   const handleBlur = (e: ChangeEvent<HTMLInputElement>) => {
     InputHandleChange(e);
@@ -43,23 +42,102 @@ const Mask = ({
       ? moment(value).locale("fa").format("jYYYY/jMM/jDD")
       : moment(value).format("YYYY/MM/DD");
 
-  const inputMask = locale === "fa" ? "0000/00/00" : "0000/00/00";
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.name, formattedValue);
+
+    let value = e.target.value.replace(/\D/g, "");
+    if (value.length > 8) value = value.slice(0, 8);
+
+    let formatted = "";
+    if (value.length > 0) {
+      formatted += value.substring(0, 4);
+    }
+    if (value.length >= 3) {
+      formatted += "/" + value.substring(4, 6);
+    }
+    if (value.length >= 5) {
+      formatted += "/" + value.substring(6, 8);
+    }
+    console.log(formatted);
+
+    setValue(formatted);
+    const checkFormatter = formatted.split("/");
+    // چک می‌کنیم که آیا تاریخ درست است یا خیر
+    const isValid = moment(
+      `${checkFormatter[0]}/${checkFormatter[1]}/${checkFormatter[2]}`,
+      "jYYYY/jMM/jDD",
+      true
+    ).isValid();
+
+    if (isValid) {
+      // اگر تاریخ معتبر باشد، آن را تنظیم می‌کنیم
+      // setValue(newValue);
+      console.log(isValid);
+    }
+    // } else {
+    //   setValue(newValue);
+    // }
+  };
+
+  // این متد برای اضافه کردن `/` در مکان صحیح است
+  // const formatInputValue = (value: string) => {
+  //   // اولین `/` پس از 4 رقم، دومین `/` پس از 6 رقم
+  //   if (value.length >= 5) {
+  //     value = `${value.substring(0, 4)}/${value.substring(4)}`;
+  //   }
+  //   if (value.length >= 8) {
+  //     value = `${value.substring(0, 7)}/${value.substring(7)}`;
+  //   }
+  //   console.log(value);
+
+  //   return value;
+  // };
 
   return (
-    <>
-      {isEdit ? (
-        <MaskedInput
-          ref={editor}
-          value={formattedValue}
-          mask={inputMask}
-          className="w-28 text-center"
-          style={{
-            fontFamily: "IRANSans",
-            borderWidth: "1px",
-            borderColor: secondaryColor,
-            color: tertiaryColor,
-          }}
-          onBlur={handleBlur}
+    <div className="" style={{ display: "flex", border: "1px solid red" }}>
+      <input
+        type="number"
+        // id="date-input"
+        name="year"
+        value={value}
+        onChange={handleChange}
+        maxLength={4} // طول ورودی محدود به 10 کاراکتر است
+        // placeholder={formatInputValue(value)}
+        className=""
+        style={{ border: "1px solid red" }}
+      />
+      <input
+        type="number"
+        // id="date-input"
+        name="month"
+        value={value}
+        onChange={handleChange}
+        maxLength={2} // طول ورودی محدود به 10 کاراکتر است
+        // placeholder={formatInputValue(value)}
+        className=""
+        style={{ border: "1px solid red" }}
+      />
+      <input
+        type="number"
+        // id="date-input"
+        name="day"
+        value={value}
+        onChange={handleChange}
+        maxLength={2} // طول ورودی محدود به 10 کاراکتر است
+        // placeholder={formatInputValue(value)}
+        className=""
+        style={{ border: "1px solid red" }}
+      />
+      {/* {isEdit ? (
+        <input
+          type="text"
+          // id="date-input"
+          // value={formatInputValue(value)}
+          // onChange={handleChange}
+          maxLength={10} // طول ورودی محدود به 10 کاراکتر است
+          placeholder={formatInputValue(value)}
+          className=""
+          style={{ border: "1px solid red", background: "#f00" }}
         />
       ) : (
         <div
@@ -67,15 +145,15 @@ const Mask = ({
             borderColor:
               dateFromOutside?.from > dateFromOutside?.to ? dangerColor : "",
           }}
-          className={`${className}  `}
+          className={`${className}`}
           onClick={() => {
             setIsEdit(true);
           }}
         >
           {formattedValue}
         </div>
-      )}
-    </>
+      )} */}
+    </div>
   );
 };
-export default Mask;
+export default DateMask;
