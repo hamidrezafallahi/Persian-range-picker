fd4c14c5e3b3a15a766f8d26d31a30a464f05a32
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-05-13 13:10:12 +0330

    mask

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
-import Range from "./range";
-import DateMask from "./range/core/mask";
+import { Range, DateMask } from "./range";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
