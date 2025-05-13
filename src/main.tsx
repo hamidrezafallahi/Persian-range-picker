732116498064538be9e86fb6e83cfff3fa152579
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-05-13 13:02:42 +0330

    mask

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import Range from "./range";
+import DateMask from "./range/core/mask";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Range device="desktop" locale="fa" model="date" />
-    {/* <JalaaliDateInput /> */}
+    <DateMask />
   </StrictMode>
 );
