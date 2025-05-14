7bf8213a18d26a08f5127fc599b1c2e3b24f0e02
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-05-14 17:54:32 +0330

    fix build error

--- src/initialComponent/index.tsx
+++ src/initialComponent/index.tsx
@@ -104,9 +104,7 @@ export function InitialComponent() {
           handleReject={resetDate}
         />
         <DateMask onChange={handleChange} />
-        <div className="p-3 border">test</div>
       </div>
-      <button>template</button>
     </>
   );
 }
