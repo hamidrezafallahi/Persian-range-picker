fe0ea470df82a4a7ffc3335c71cf3e61c0f1a00b
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-05-21 17:06:12 +0330

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import './range.min.css';
+import "./range.min.css";
 
 // import { StrictMode } from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-import { InitialComponent } from './initialComponent';
+import { InitialComponent } from "./initialComponent";
 
 createRoot(document.getElementById("root")!).render(
   <>
     <InitialComponent />
+
+    <p>test33333333333</p>
   </>
 );
