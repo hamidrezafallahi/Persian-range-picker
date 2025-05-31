35a2361754f5dfedad08a752fef9ad4f1fa6be18
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-05-31 11:42:18 +0330

    fix date in mobile height 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-    server: {
-    host: '0.0.0.0',
-    port: 5173 // or any open port
+  server: {
+    host: "0.0.0.0",
+    port: 5173, // or any open port
   },
-base:'Persian-range-picker',
+  base: "/Persian-range-picker",
   plugins: [react()],
-})
+});
