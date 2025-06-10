18b8f244e507981d23c95fbc53df165d5a9e6cb5
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-06-10 10:58:12 +0330

    fix border

--- src/range/core/mainContent.tsx
+++ src/range/core/mainContent.tsx
@@ -25,7 +25,7 @@ const MainContent = ({ ...props }: IProps) => {
     periodListClassName,
     tabClassName,
     accentColor = "#2563eb",
-    locale,
+    locale = "fa",
     setStep,
     setZone,
     setActiveCompareStep,
@@ -84,46 +84,36 @@ const MainContent = ({ ...props }: IProps) => {
   return (
     <div
       dir={locale == "fa" ? "rtl" : "ltr"}
-      className={`flex flex-col xs:!flex-row xs:h-full `}
+      className={"flex flex-col xs:flex-row p-1 xs:h-full"}
     >
       <div
-        className={`flex justify-around  xs:flex-col  xs:gap-9 p-2  border-gray-300 !border-b xs:!border-b-0 ${
-          locale == "fa" ? "xs:!border-l" : "xs:!border-r"
-        } xs:w-28  h-10 xs:!h-[calc(100%-52px)] xs:overflow-y-auto overflow-x-auto ${tabClassName} `}
+        className={` flex xs:flex-col border-b   p-1 w-full xs:overflow-y-auto xs:w-28  justify-around gap-9 p-2  max-w-[430px] xs:h-10 overflow-x-auto"
+    }
+    ${locale !== "fa" ? "xs:border-r" : "xs:border-l"}
+    ${tabClassName}
+  `.trim()}
       >
         {tabs.map((tab) => (
           <button
             style={{
               color: activeTab === tab.key ? accentColor : "",
-              border: activeTab === tab.key ? accentColor : "",
+              // border: activeTab === tab.key ? accentColor : "",
             }}
             key={tab.key}
             onClick={() => handleTabChange(tab.key)}
-            className={`${periodListClassName} font-medium text-right text-nowrap text-sm  *:
+            className={`${periodListClassName} font-medium text-right text-nowrap text-sm 
             ${
               activeTab === tab.key
-                ? "border-b-2 xs:border-none"
+                ? locale == "fa"
+                  ? "border-b-2  xs:border-r-2 "
+                  : "border-b-2  xs:border-l-2 "
                 : "  text-gray-500 hover:text-gray-700"
             }
-            flex justify-center xs:!justify-start xs:gap-3 items-center
+            flex justify-center xs:justify-start xs:gap-3 items-center
             
             `}
           >
-            <div
-              style={{
-                color: activeTab === tab.key ? accentColor : "text-gray-500",
-                borderColor: activeTab === tab.key ? accentColor : "",
-              }}
-              className={`h-8  hidden xs:block
-                ${
-                  activeTab === tab.key
-                    ? "border-r-4"
-                    : "   hover:text-gray-700"
-                }`}
-            >
-              {" "}
-            </div>
-            <div> {tab.label}</div>
+            {tab.label}
           </button>
         ))}
       </div>
