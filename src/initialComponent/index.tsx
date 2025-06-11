8480984b025f9d896fb6869eef343473e6f9aa5b
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-06-11 13:44:39 +0330

    build success

--- src/initialComponent/index.tsx
+++ src/initialComponent/index.tsx
@@ -6,100 +6,100 @@ import {
   MobileRange,
   TimePicker,
 } from "../range";
-import type { IDate } from "../range/core/type";
+// import type { IDate } from "../range/core/type";
 
 export function InitialComponent() {
-  const handleDateChange = (w) => {
-    if (w?.date?.from) {
-      console.log(
-        new Date(w.date.from).toLocaleDateString("fa-IR", {
-          weekday: "long",
-          month: "long",
-          day: "numeric",
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-        })
-      );
-    }
-    // new Date(w?.to).toLocaleDateString("fa-IR", {
-    //   weekday: "long",
-    //   month: "long",
-    //   day: "numeric",
-    //   hour: "numeric",
-    // })
-  };
+  // const handleDateChange = (w) => {
+  //   if (w?.date?.from) {
+  //     console.log(
+  //       new Date(w.date.from).toLocaleDateString("fa-IR", {
+  //         weekday: "long",
+  //         month: "long",
+  //         day: "numeric",
+  //         hour: "numeric",
+  //         minute: "numeric",
+  //         second: "numeric",
+  //       })
+  //     );
+  //   }
+  //   // new Date(w?.to).toLocaleDateString("fa-IR", {
+  //   //   weekday: "long",
+  //   //   month: "long",
+  //   //   day: "numeric",
+  //   //   hour: "numeric",
+  //   // })
+  // };
 
-  const handleCompareDateChange = (date: IDate, compareDate: IDate) => {
-    console.log(
-      "date : ",
-      new Date(date?.from).toLocaleDateString("fa-IR", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-      }),
-      new Date(date?.to).toLocaleDateString("fa-IR", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-      }),
-      "compare date : ",
-      new Date(compareDate?.from).toLocaleDateString("fa-IR", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-      }),
-      new Date(compareDate?.to).toLocaleDateString("fa-IR", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-      })
-    );
-  };
+  // const handleCompareDateChange = (date: IDate, compareDate: IDate) => {
+  //   console.log(
+  //     "date : ",
+  //     new Date(date?.from).toLocaleDateString("fa-IR", {
+  //       weekday: "long",
+  //       month: "long",
+  //       day: "numeric",
+  //       hour: "numeric",
+  //     }),
+  //     new Date(date?.to).toLocaleDateString("fa-IR", {
+  //       weekday: "long",
+  //       month: "long",
+  //       day: "numeric",
+  //       hour: "numeric",
+  //     }),
+  //     "compare date : ",
+  //     new Date(compareDate?.from).toLocaleDateString("fa-IR", {
+  //       weekday: "long",
+  //       month: "long",
+  //       day: "numeric",
+  //       hour: "numeric",
+  //     }),
+  //     new Date(compareDate?.to).toLocaleDateString("fa-IR", {
+  //       weekday: "long",
+  //       month: "long",
+  //       day: "numeric",
+  //       hour: "numeric",
+  //     })
+  //   );
+  // };
 
-  const handleNavigateChange = (date: IDate, compareDate: IDate | null) => {
-    console.log(
-      "date : ",
-      new Date(date?.from).toLocaleDateString("fa-IR", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-      }),
-      new Date(date?.to).toLocaleDateString("fa-IR", {
-        weekday: "long",
-        month: "long",
-        day: "numeric",
-        hour: "numeric",
-      }),
-      "compare date : ",
-      compareDate &&
-        new Date(compareDate.from).toLocaleDateString("fa-IR", {
-          weekday: "long",
-          month: "long",
-          day: "numeric",
-          hour: "numeric",
-        }),
-      compareDate &&
-        new Date(compareDate.to).toLocaleDateString("fa-IR", {
-          weekday: "long",
-          month: "long",
-          day: "numeric",
-          hour: "numeric",
-        })
-    );
-  };
+  // const handleNavigateChange = (date: IDate, compareDate: IDate | null) => {
+  //   console.log(
+  //     "date : ",
+  //     new Date(date?.from).toLocaleDateString("fa-IR", {
+  //       weekday: "long",
+  //       month: "long",
+  //       day: "numeric",
+  //       hour: "numeric",
+  //     }),
+  //     new Date(date?.to).toLocaleDateString("fa-IR", {
+  //       weekday: "long",
+  //       month: "long",
+  //       day: "numeric",
+  //       hour: "numeric",
+  //     }),
+  //     "compare date : ",
+  //     compareDate &&
+  //       new Date(compareDate.from).toLocaleDateString("fa-IR", {
+  //         weekday: "long",
+  //         month: "long",
+  //         day: "numeric",
+  //         hour: "numeric",
+  //       }),
+  //     compareDate &&
+  //       new Date(compareDate.to).toLocaleDateString("fa-IR", {
+  //         weekday: "long",
+  //         month: "long",
+  //         day: "numeric",
+  //         hour: "numeric",
+  //       })
+  //   );
+  // };
 
-  const HandleSubmitDate = (w: IDate, e: IDate | null) => {
-    console.log(w, e);
-  };
-  const resetDate = () => {
-    console.log("Reject");
-  };
+  // const HandleSubmitDate = (w: IDate, e: IDate | null) => {
+  //   console.log(w, e);
+  // };
+  // const resetDate = () => {
+  //   console.log("Reject");
+  // };
   const handleChange = (e: number | null) => {
     console.log(e);
   };
@@ -118,7 +118,7 @@ export function InitialComponent() {
           handleReject={resetDate}
         /> */}
         <div className="flex flex-col" dir="rtl">
-          <DesktopDate onChange={handleDateChange} />
+          <DesktopDate />
           <DesktopRange />
           <MobileDate />
           <MobileRange />
