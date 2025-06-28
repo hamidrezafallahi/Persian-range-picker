3962535150fcbf5c8a16bbaaebc6a94a9aacaf8d
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-06-28 09:18:39 +0330

    UPDATE

--- src/demoComponent/index.tsx
+++ src/demoComponent/index.tsx
@@ -2,21 +2,11 @@ import { DatePicker, RangePicker, TimePicker, Mask } from "../range";
 
 export default function DemoComponent() {
   return (
-<<<<<<< HEAD
     <>
-        <RangePicker  />
-        <DatePicker showTime />
-        <TimePicker  />
-        <Mask />
-    </>
-=======
-    <div>
-      {/* <div style={{ width: "40%" }}></div> */}
       <RangePicker />
-      <DatePicker />
+      <DatePicker showTime />
       <TimePicker />
       <Mask />
-    </div>
->>>>>>> 83a6085489f0de004b2eb7511bc3820686d17084
+    </>
   );
 }
