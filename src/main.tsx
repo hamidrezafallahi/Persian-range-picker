6e66e21b88283a1c6f0ca18ee59f2ec541e8dba3
hamidreza fallahi <hamidreza.lipar@gmail.com>
2025-06-30 17:01:04 +0330

    fix export types

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./main.css";
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import DemoComponent from "./demoComponent";
+// import DemoComponent from "./demoComponent";
 import { InitialComponent } from "./initialComponent";
 
 // import { InitialComponent } from "./initialComponent";
