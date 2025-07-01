12c1147b8d3cf3f9d81f4fea402a65a15df439c8
f.abdolmaleki <fatemeabdolmaleki703@gmail.com>
2025-07-01 13:30:20 +0330

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-// import { InitialComponent } from "./initialComponent";
 import './main.css';
 
 // import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client';
 
-import DemoComponent from './demoComponent';
+// import { InitialComponent } from "./initialComponent";
+import { InitialComponent } from './initialComponent';
+
+// import DemoComponent from './demoComponent';
 
 // import { InitialComponent } from "./initialComponent";
 
 createRoot(document.getElementById("root")!).render(
   <>
-    {/* <InitialComponent /> */}
-    <DemoComponent />
+    <InitialComponent />
+    {/* <DemoComponent /> */}
   </>
 );
