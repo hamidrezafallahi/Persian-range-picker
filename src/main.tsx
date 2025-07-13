38a0831854c4e4290a5f7caae3d7a0bda85b9643
farinazSoleimani <soleimanyfarinaz71@gmail.com>
2025-07-13 18:40:28 +0330

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
-import "./main.css";
+import "./main.module.css";
+
 import { createRoot } from "react-dom/client";
+
 import DemoComponent from "./demoComponent";
+
 // import { InitialComponent } from "./initialComponent";
 createRoot(document.getElementById("root")!).render(
   <>
