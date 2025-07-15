ef2734ad24fc9a3a45eb06184ba8a04ed25be012
f.abdolmaleki <fatemeabdolmaleki703@gmail.com>
2025-07-15 10:25:40 +0330

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import "./main.css";
+
 import { createRoot } from "react-dom/client";
-import DemoComponent from "./demoComponent";
+
+import { InitialComponent } from "./initialComponent";
+
 // import { InitialComponent } from "./initialComponent";
 createRoot(document.getElementById("root")!).render(
   <>
-    {/* <InitialComponent /> */}
-    <DemoComponent />
+    <InitialComponent />
+    {/* <DemoComponent /> */}
   </>
 );
