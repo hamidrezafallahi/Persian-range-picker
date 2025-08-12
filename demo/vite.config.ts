cdbe42518596d5bc73f352f2bf876d3b76ef793f
f.abdolmaleki <fatemeabdolmaleki703@gmail.com>
2025-08-12 11:16:49 +0330

    create demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,20 +1,15 @@
 import path from "path";
-import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react-swc";
 
-// این دو خط برای تعریف __dirname در ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 export default defineConfig({
   root: __dirname,
-  base: "/Persian-range-picker/", // اسم ریپوی خودت
+  base: "/Persian-range-picker/",
   plugins: [react()],
   resolve: {
     alias: {
-      "@lib": path.resolve(__dirname, "../src"), // مسیر src اصلی
+      "@lib": path.resolve(__dirname, "../src"),
     },
   },
 });
