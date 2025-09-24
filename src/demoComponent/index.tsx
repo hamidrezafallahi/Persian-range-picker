38e95859d86b06c98f5b5ddf384192de3b786f1a
h.falahi <hamidreza.lipar@gmail.com>
2025-09-24 10:57:03 +0330

    mobile popover to create portal

--- src/demoComponent/index.tsx
+++ src/demoComponent/index.tsx
@@ -1,70 +1,66 @@
-import style from '../main.module.css';
-import {
-  DatePicker,
-  Mask,
-  RangePicker,
-} from '../range';
+import { DatePicker } from '../range';
 
 export default function DemoComponent() {
   return (
-    <div className={style.bg_red_100} >
-      <RangePicker
-      showComparison
-        onChange={(e) => {
-          console.log("RangePicker has changed", e);
-        }}
-      />
+            <DatePicker/>
+    // <div className={style.bg_red_100} >
+    //   <RangePicker
+    //   showComparison
+    //     onChange={(e) => {
+    //       console.log("RangePicker has changed", e);
+    //     }}
+    //   />
 
-      {/* <div className={`${style.bg_red_400}`} style={{ height: "1000px" }}>
-        height
-      </div> */}
-      {/* 
-      <div className={`${style.flex}`}>
-        <div className={`${style.bg_red_400}`} style={{ width: "500px" }}>
-          test
-        </div> */}
+    //   {/* <div className={`${style.bg_red_400}`} style={{ height: "1000px" }}>
+    //     height
+    //   </div> */}
+    //   {/* 
+    //   <div className={`${style.flex}`}>
+    //     <div className={`${style.bg_red_400}`} style={{ width: "500px" }}>
+    //       test
+    //     </div> */}
 
-      <div>
-        <DatePicker
-          // disabled
-          // showTime
-          // showSecond
-          showMask
-          // isTodaySelectPreset
-          // defaultValue={1754625824000}
-          defaultValue={"2025-05-05T00:00:00.000+03:30"}
-          onChange={(e) => {
-            console.log(e);
-          }}
-          calendarType="shamsi"
-          // isOpenDropdown
-        />
-      </div>
-      {/* </div> */}
-      {/* <TimePicker
-        disabled
-        onChange={(e) => {
-          console.log(e);
-        }}
-        // showSecond
-        // exportType="timeStamp"
-      /> */}
+    //   <div>
+    //     <DatePicker
+    //       // disabled
+    //       // showTime
+    //       // showSecond
+    //       showMask
+    //       // isTodaySelectPreset
+    //       // defaultValue={1754625824000}
+    //       defaultValue={"2025-05-05T00:00:00.000+03:30"}
+    //       onChange={(e) => {
+    //         console.log(e);
+    //       }}
+    //       calendarType="shamsi"
+    //       // isOpenDropdown
+    //     />
+    //   </div>
+    //   {/* </div> */}
+    //   {/* <TimePicker
+    //     disabled
+    //     onChange={(e) => {
+    //       console.log(e);
+    //     }}
+    //     // showSecond
+    //     // exportType="timeStamp"
+    //   /> */}
 
-      <Mask
-        // MaskFontStyle={{
-        //   fontFamily: "sans-serif",
-        //   fontSize: 10,
-        //   color: "red",
-        // }}
-        // disabled
-        // isTodaySelectPreset
-        onMaskChange={(e) => {
-          console.log("mask has changed", e);
-        }}
-        // exportType="timeStamp"
-        // maskFontSize={}
-        // maskFontFamily=""
-      />
-    </div>
+    //   <Mask
+    //     // MaskFontStyle={{
+    //     //   fontFamily: "sans-serif",
+    //     //   fontSize: 10,
+    //     //   color: "red",
+    //     // }}
+    //     // disabled
+    //     // isTodaySelectPreset
+    //     onMaskChange={(e) => {
+    //       console.log("mask has changed", e);
+    //     }}
+    //     // exportType="timeStamp"
+    //     // maskFontSize={}
+    //     // maskFontFamily=""
+    //   />
+    // </div>
   );
 }
